075d7a8ab4dda6f2e62914f425ccd0f615b12823
Alex <easyfreedom44@gmail.com>
2026-05-13 16:55:58 +0300

    HW_2_add_InMemoryRepository

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryUserRepository.java
@@ -6,40 +6,56 @@ import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepository implements UserRepository {
+    private final Map<Integer, User> userMap = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
 
     @Override
-    public boolean delete(int id) {
-        log.info("delete {}", id);
-        return true;
-    }
-
-    @Override
     public User save(User user) {
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+            userMap.put(user.getId(), user);
+        }
+        userMap.computeIfPresent(user.getId(), (id, oldUser) -> user);
         log.info("save {}", user);
         return user;
     }
 
     @Override
+    public boolean delete(int id) {
+        log.info("delete {}", id);
+        return userMap.remove(id) != null;
+    }
+
+    @Override
     public User get(int id) {
         log.info("get {}", id);
-        return null;
+        return userMap.get(id);
     }
 
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return userMap.values().stream().sorted(Comparator.comparing(User::getName)).collect(Collectors.toList());
     }
 
     @Override
     public User getByEmail(String email) {
         log.info("getByEmail {}", email);
+        for (User user : userMap.values()) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
         return null;
     }
 }
