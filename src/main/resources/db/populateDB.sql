8d00723e6fbe003d062de16fa1c803a9c83654ee
Alex <easyfreedom44@gmail.com>
2026-05-30 15:57:43 +0300

    HW_3 add table meals on DB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,4 @@
-DELETE FROM user_role;
-DELETE FROM users;
+TRUNCATE TABLE meals, user_role, users RESTART IDENTITY CASCADE;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +9,8 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2025-05-30 10:00:00', 'Завтрак', 500),
+       (100000, '2025-05-30 13:00:00', 'Обед', 1000),
+       (100001, '2025-05-30 09:30:00', 'Кофе', 300);
