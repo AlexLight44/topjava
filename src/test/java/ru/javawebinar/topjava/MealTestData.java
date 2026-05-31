1aba99ad89339db5d32c80eba1cc0a565334061a
Alex <easyfreedom44@gmail.com>
2026-05-31 15:38:58 +0300

    HW_3 add MealServiceTest and MealTestData

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,46 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
+    public static final int MEAL_ID = START_SEQ + 3;
+
+
+    public static final Meal meal1 = new Meal(MEAL_ID, LocalDateTime.of(2026, 5, 30, 10, 0), "Завтрак", 500);
+    public static final Meal meal2 = new Meal(MEAL_ID + 1, LocalDateTime.of(2026, 5, 30, 13, 0), "Обед", 1000);
+
+    public static Meal getNew() {
+        return new Meal(null, LocalDateTime.now(), "newMeal", 600);
+    }
+
+    public static Meal getUpdated() {
+        Meal updated = new Meal(meal1);
+        updated.setDateTime(LocalDateTime.of(2026, 5, 29, 12, 12));
+        updated.setDescription("updateMeal");
+        updated.setCalories(100);
+        return updated;
+
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).usingRecursiveComparison().ignoringFields("id", "dateTime").isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id", "dateTime")
+                .isEqualTo(expected);
+    }
+}
